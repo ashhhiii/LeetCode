57a24d4b20348da348bd7ebba629a950986eec97
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-07-12 22:40:57 +0530

    Time: 23 ms (24.63%) | Memory: 53 MB (5.04%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,17 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-         int prefixSum = 0;
+        int prefixSum = 0;
         int count = 0;
         HashMap<Integer, Integer> countMap = new HashMap<>();
-        countMap.put(0, 1); // To handle subarrays that start from the beginning
+        countMap.put(0, 1);
 
         for (int num : nums) {
             prefixSum += num;
 
-            // Check if prefixSum - goal exists in the map
             if (countMap.containsKey(prefixSum - goal)) {
                 count += countMap.get(prefixSum - goal);
             }
 
-            // Update the frequency of the current prefixSum
             countMap.put(prefixSum, countMap.getOrDefault(prefixSum, 0) + 1);
         }
 
