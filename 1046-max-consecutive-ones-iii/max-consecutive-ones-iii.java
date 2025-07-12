ac0fd43200f15ac6f01e7c1907bbd46209c4cd8c
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-07-12 22:11:23 +0530

    Time: 3 ms (94.04%) | Memory: 46.9 MB (14.70%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,28 +1,23 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-     int left = 0; // Left pointer of the sliding window
-        int maxLength = 0; // Maximum length of the subarray
-        int zeroCount = 0; // Count of 0s in the current window
+        int n = nums.length;
+        int left = 0;
+        int maxLength= 0;
+        int zeroCount = 0;
 
-        for (int right = 0; right < nums.length; right++) {
-            // If the current number is 0, increase zeroCount
-            if (nums[right] == 0) {
+        for( int right = 0; right < n ; right++){
+            if(nums[right] == 0){
                 zeroCount++;
             }
-
-            // Shrink the window from the left if zeroCount exceeds k
-            while (zeroCount > k) {
-                if (nums[left] == 0) {
+            while(zeroCount > k){
+                if(nums[left] == 0){
                     zeroCount--;
                 }
                 left++;
             }
+             maxLength = Math.max(maxLength , right-left + 1);
 
-            // Update the maximum length of the window
-            maxLength = Math.max(maxLength, right - left + 1);
         }
-
         return maxLength;
     }
-
 }
