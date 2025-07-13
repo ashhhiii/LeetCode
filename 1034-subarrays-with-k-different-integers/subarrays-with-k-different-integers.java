c3f293a522e27fb983f369a8ca923b6fd92a0eec
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-07-13 21:41:57 +0530

    Time: 49 ms (52.93%) | Memory: 47.9 MB (33.60%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,25 +1,24 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMost(nums,k) - atMost(nums,k-1);
+        return atmost(nums,k) - atmost(nums,k-1);
     }
-    private int atMost(int[] nums,int k){
-        HashMap<Integer,Integer> countMap = new HashMap<>();
-        int left=0;
-        int subArray = 0;
+    private int atmost(int[] nums, int k){
+        int left = 0;
+        int count= 0 ;
+        HashMap<Integer , Integer> countMap = new HashMap<>();
 
-        for(int right=0;right<nums.length;right++){
-            countMap.put(nums[right],countMap.getOrDefault(nums[right],0)+1);
-            
-           while (countMap.size() > k) {
-                countMap.put(nums[left], countMap.get(nums[left]) - 1);
-                if (countMap.get(nums[left]) == 0) {
-                    countMap.remove(nums[left]);
-                }
-                left++;
+        for(int right = 0; right < nums.length; right++){
+            countMap.put(nums[right] , countMap.getOrDefault(nums[right] , 0)+1);
+        
+        while(countMap.size() > k){
+            countMap.put(nums[left] , countMap.get(nums[left]) -1);
+            if(countMap.get(nums[left]) == 0){
+                countMap.remove(nums[left]);
             }
-
-        subArray +=right-left+1;
+            left++;
         }
-        return subArray;
+        count += right - left +1;
+    }
+    return count;
     }
 }
