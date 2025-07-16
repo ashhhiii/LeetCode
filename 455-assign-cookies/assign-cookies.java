d696dac381c6fd544566bcb05481a995e0152668
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-07-16 21:54:33 +0530

    Time: 10 ms (79.73%) | Memory: 48.8 MB (24.89%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,16 +1,19 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
+         Arrays.sort(g);
         Arrays.sort(s);
-        int contentChild =0;
+
+        int contentChild = 0;
         int cookieSize = 0;
-        while(contentChild<g.length && cookieSize < s.length){
-            if(s[cookieSize] >= g[contentChild]){
+
+        while (contentChild < g.length && cookieSize < s.length) {
+            if (s[cookieSize] >= g[contentChild]) {
                 contentChild++;
             }
             cookieSize++;
-
         }
-         return contentChild;
+
+        return contentChild;
+    
     }
 }
