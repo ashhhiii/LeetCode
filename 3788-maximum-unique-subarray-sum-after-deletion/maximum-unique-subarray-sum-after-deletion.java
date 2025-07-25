bf6c69cfbc2caa533fd09971547d842ea0a97545
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-07-25 21:02:59 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (64.69%) - LeetSync

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        int[] mp = new int[101];
+        Arrays.fill(mp, -1);
+
+        int sum = 0;
+        int maxNeg = Integer.MIN_VALUE;
+
+        for(int num: nums){
+            if(num <= 0){
+                maxNeg = Math.max(maxNeg ,  num);
+            }else if(mp[num] == -1){
+                sum += num;
+                mp[num] = 1;
+            }
+        }
+        return sum > 0 ? sum: maxNeg;
+    }
+}
