c8c812956d21a91ffc4d04a8149fe87b1baed827
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-07-30 00:58:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (97.38%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -8,15 +8,14 @@ class Solution {
     }
 
     private void dfs(int[][] image, int x, int y, int originalColor, int newColor) {
-        // Boundary check
+        
         if (x < 0 || y < 0 || x >= image.length || y >= image[0].length || image[x][y] != originalColor) {
             return;
         }
 
-        // Change the color
         image[x][y] = newColor;
 
-        // Move in 4 directions (up, down, left, right)
+     
         dfs(image, x + 1, y, originalColor, newColor);
         dfs(image, x - 1, y, originalColor, newColor);
         dfs(image, x, y + 1, originalColor, newColor);
