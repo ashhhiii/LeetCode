9f19a43b114eadecd802a9f3114af33738621da4
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-07-30 22:11:12 +0530

    Time: 6 ms (98.81%) | Memory: 56.9 MB (50.21%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -2,7 +2,7 @@ class Solution {
     public int numEnclaves(int[][] grid) {
      int m = grid.length, n = grid[0].length;
 
-        // Step 1: Mark all boundary land cells and connected cells as water (0)
+        
         for (int i = 0; i < m; i++) {
             if (grid[i][0] == 1) dfs(grid, i, 0);
             if (grid[i][n - 1] == 1) dfs(grid, i, n - 1);
@@ -12,7 +12,6 @@ class Solution {
             if (grid[m - 1][j] == 1) dfs(grid, m - 1, j);
         }
 
-        // Step 2: Count remaining land cells that are completely enclosed
         int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -30,10 +29,10 @@ class Solution {
             return;
         }
 
-        // Mark as visited by changing land to water (1 → 0)
+     
         grid[i][j] = 0;
 
-        // Explore all four directions
+        
         dfs(grid, i + 1, j);
         dfs(grid, i - 1, j);
         dfs(grid, i, j + 1);
