c700749c85fcc289865eb3c46177815f722768b0
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-08-01 23:17:50 +0530

    Time: 1 ms (82.66%) | Memory: 41.9 MB (91.00%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-         List<List<Integer>> triangle = new ArrayList<>();
+        List<List<Integer>> triangle = new ArrayList<>();
 
         if (numRows <= 0) {
             return triangle;
         }
 
-        // Generate rows of Pascal's Triangle
+        
         for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
-            // Each row starts and ends with 1
+     
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
                     row.add(1);
                 } else {
-                    // Sum of the two numbers above
+                
                     row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
                 }
             }
@@ -23,5 +23,4 @@ class Solution {
 
         return triangle;
     }
-    
 }
