b815aa04a8baef366cd79db56ab6af2649bcd275
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-08-20 20:23:22 +0530

    Time: 27 ms (93.20%) | Memory: 47.1 MB (29.06%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -5,7 +5,7 @@ class Solution {
         int[] minCuts = new int[n];
 
         for (int end = 0; end < n; end++) {
-            int min = end; // max cuts = end (cut before each character)
+            int min = end; 
             for (int start = 0; start <= end; start++) {
                 if (s.charAt(start) == s.charAt(end) &&
                     (end - start <= 2 || isPalindrome[start + 1][end - 1])) {
@@ -13,7 +13,8 @@ class Solution {
                     isPalindrome[start][end] = true;
 
                     if (start == 0) {
-                        min = 0; // no cut needed if s[0..end] is a palindrome
+                        min = 0; 
+                        
                     } else {
                         min = Math.min(min, minCuts[start - 1] + 1);
                     }
