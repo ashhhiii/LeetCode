3119dc4a0013493c00d13ce181a8f4729a67d2a3
Ashi Chauda <140250155+ashhhiii@users.noreply.github.com>
2025-08-31 11:43:57 +0530

    Time: 1 ms (99.63%) | Memory: 45.9 MB (86.23%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 0) {
+            return;
+        }
+
+        int nonZeroPos = 0; 
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroPos] = nums[i];
+                nonZeroPos++;
+            }
+        }
+
+        for (int i = nonZeroPos; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
